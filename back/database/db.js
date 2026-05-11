f349543c6cec3fad6824dd5bd63c590a058fe7b3
mauro <andresmao205@gmail.com>
2026-05-10 22:58:54 -0500

    Refactor: cambio conexion db temp

--- back/database/db.js
+++ back/database/db.js
@@ -1,13 +1,29 @@
+// import pkg from 'pg';
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//     user: 'mauro',
+//     password: 'mauro',
+//     host: 'localhost',
+//     port: 3000,
+//     database: 'antojitos_db'
+// });
+
+
 import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: 'mauro',
-    password: 'mauro',
-    host: 'localhost',
-    port: 3000,
-    database: 'antojitos_db'
+    user: 'postgres',
+    password: 'Andresmao1205',
+    host: 'db.vhwrwnizskcbupmvykrx.supabase.co',
+    port: 5432,
+    database: 'postgres',
+    ssl: {
+        rejectUnauthorized: false,
+    }
 });
+
 
 
 export { pool }
